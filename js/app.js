9d2766d277c1434042bf4979fb411ede66678352
James Mendenhall <Nattysoccer9@yahoo.com>
2017-10-05 09:26:56 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+  $(function() {
+    var API = 'http://localhost:8080/book'
+    $.get(API).then(function(data) {
+      for (var i = 0; i < data.length; i++) {
+        var imgUrl = data[i].bookURL
+        var title = data[i].title
+        var description = data[i].description
+        var genre = data[i].genre
+        $(".bookPic").append(`
+          <div class="row">
+          <div class="col s14 m4 l4">
+            <div class="card ">
+            <div class="card-image">
+              <img src=${imgUrl} class="maxHeight366">
+            </div>
+            <div class="card-action">
+              <a href="#" class="black-text">View Books</a>
+          </div>
+          </div>
+        </div>
+        <div class="col s8 m8 l8">
+          <div class="card ">
+            <div class="card-content ">
+            <span class="card-title">${title}</span>
+              <p>${description}</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Edit</a>
+              <a href="#">Delete</a>
+            </div>
+            </div>
+          </div>
+            </div>
+        `)
+      }
+    });
+  })
+  $(function() {
+    var API = 'http://localhost:8080/author'
+    $.get(API).then(function(data) {
+      for (var i = 0; i < data.length; i++) {
+        var name = data[i].name
+        var biography = data[i].biography
+        var portrait = data[i].portrait
+        $(".authorInfo").append(`
+          <div class="row">
+          <div class="col s14 m4 l4">
+            <div class="card ">
+            <div class="card-image">
+              <img src=${portrait} class="maxHeight366">
+            </div>
+            <div class="card-action">
+              <a href="#" class="black-text">View Books</a>
+          </div>
+          </div>
+        </div>
+        <div class="col s8 m8 l8">
+          <div class="card ">
+            <div class="card-content ">
+            <span class="card-title">${name}</span>
+              <p>${biography}</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Edit</a>
+              <a href="#">Delete</a>
+            </div>
+            </div>
+          </div>
+            </div>
+        `)
+      }
+    });
+  })
+})
